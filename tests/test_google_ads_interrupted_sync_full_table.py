fbe2b3f7aa82f624d608ab80dad977f65ebff380
Csaba Jarai <csaba@tectrac.com>
2025-08-21 16:33:06 +0300

    Upgrade API version to v20 * Updates API version to 21 * Updates pkg version to 27.0.0 * Updates `singer-python` to 6.1.1 * Remove deprecated streams * Based on https://github.com/TTCsaba/tap-google-ads/commit/ba12542a1110c87323e1c9656d791c58b29ee5f3

--- tests/test_google_ads_interrupted_sync_full_table.py
+++ tests/test_google_ads_interrupted_sync_full_table.py
@@ -35,8 +35,7 @@ class InterruptedSyncFullTableTest(GoogleAdsBase):
 
         streams_under_test = {
             'ads',
-            'campaign_criterion',
-            'feed'
+            'campaign_criterion'
         }
     
         # Create connection
